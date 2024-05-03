850c8de0a6b70a328084fe3a9176ff4dba49aebd
Maheen Fatma <goodmaheen@gmail.com>
2024-05-03 14:38:10 +0530

    maheennnn

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import Header from "./assets/components/Header/Header.jsx"
 import Footer from "./assets/components/Footer/Footer.jsx"
-import { Outlet } from "react-router-dom"
+import { Outlet } from 'react-router-dom'
 
 function Layout() {
   
